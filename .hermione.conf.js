343a5a227fcf172b94be69dd428693618a6fcb5c
Syrym Umbetov <umbetov.syrym@gmail.com>
2023-07-02 17:47:33 +0600

    first commit

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -8,8 +8,13 @@ module.exports = {
   browsers: {
     chrome: {
       automationProtocol: "devtools",
+      retry: 1,
       desiredCapabilities: {
         browserName: "chrome",
+      },
+      windowSize: {
+        width: 1920,
+        height: 1080,
       },
     },
   },
---
+++ test/unit/catalog.test.tsx
@@ -0,0 +1,17 @@
+// import React from "react";
+// import { render } from "@testing-library/react";
+// import { Provider } from "react-redux";
+// import { MemoryRouter } from "react-router";
+// import { Application } from "./../../src/client/Application";
+
+// describe("Тест application", () => {
+//   it("Correct render", async () => {
+//     render(
+//       <MemoryRouter initialEntries={["/"]}>
+//         <Provider store={store}>
+//           <Application />
+//         </Provider>
+//       </MemoryRouter>
+//     );
+//   });
+// });
